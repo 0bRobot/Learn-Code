33a14b5c717f792648eac1f9070fdad01fb6a72f
MrKing <king.mryoung@gmail.com>
2022-06-19 22:56:20 +0800

    图形

--- c_code/源.cpp
+++ c_code/源.cpp
@@ -1,7 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include <stdlib.h>
 #include <graphics.h>  // 引用三方图形库头文件   
 
+void outtextxy_int(int x, int y, char *format, int iNumber)
+{
+	char str[20] = "";
+	sprintf(str,format,iNumber);
+	outtextxy(x,y,str);
+}
 
 int main()
 {
@@ -11,6 +18,7 @@ int main()
 	//纵向 为 y 轴
 	setbkcolor(LIGHTRED);//设置背景颜色
 	cleardevice();  // 设置背景颜色需要刷新
+	
 
 	putpixel(340,268,RED);  //画点    颜色宏 表示颜色  BULE  BLACK 。。。
 	for (int x = 0; x < 400; x += 2)  //for 循环画多点 
@@ -33,15 +41,18 @@ int main()
 	settextcolor(LIGHTGREEN);
 	char outst[] = "NI Hao C!!!!";
 	setbkmode(TRANSPARENT); //去掉文字背景
-	for (int i = 0; i <= 200; i += 20)
-	{
-		cleardevice(); //清除显示设备
-		settextstyle(i, 0, "Consolas");  //字体逐渐 变大
-		outtextxy(10, 60, outst);  //文字输出  使用宽字节
-		Sleep(270);
-	}
-	
-	
+	//for (int i = 0; i <= 200; i += 20)
+	//{
+	//	cleardevice(); //清除显示设备
+	//	settextstyle(i, 0, "Consolas");  //字体逐渐 变大
+	//	outtextxy(10, 60, outst);  //文字输出  使用宽字节
+	//	Sleep(270);
+	//}
+
+	settextstyle(50, 0, "Consolas");  //字体逐渐 变大
+	outtextxy(80, 350, outst);  //文字输出  使用宽字节
+
+	outtextxy_int(200, 200, "你好!% d", 1314); //数字输出
 
 	getchar(); //从窗口获取按键  防止窗口关闭
 
